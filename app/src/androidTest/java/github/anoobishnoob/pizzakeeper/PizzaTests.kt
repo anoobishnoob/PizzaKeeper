26f600ad17e19e47173ebf8de9300d0f0f1bb7ee
Samme Qandil <saqandil@gmail.com>
2018-08-26 15:43:06 -0700

    just did some testing
    need to figure out this error, but this is a lot of forward progress today

---
+++ app/src/androidTest/java/github/anoobishnoob/pizzakeeper/PizzaTests.kt
@@ -0,0 +1,31 @@
+package github.anoobishnoob.pizzakeeper
+
+import android.arch.persistence.room.Room
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import github.anoobishnoob.pizzakeeper.data.Pizza
+import github.anoobishnoob.pizzakeeper.data.PizzaDatabase
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class PizzaTests {
+    val testPizza = Pizza(0, "Hawaiian", Date())
+    val testToppingIds = listOf(1,7)
+
+
+
+    @Test
+    fun PizzaTest() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        val db = Room.databaseBuilder(appContext, PizzaDatabase::class.java,"PizzaDatabase")
+                .build()
+        db.pizzaDao().insert(testPizza)
+        val returnedPizza = db.pizzaDao().getPizzaByID(testPizza.id)
+        assertEquals(testPizza, returnedPizza)
+    }
+}
