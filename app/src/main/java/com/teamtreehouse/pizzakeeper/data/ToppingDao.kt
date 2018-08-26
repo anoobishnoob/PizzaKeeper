043db3193d8ecd332665f8711ef1db4352539fd8
Samme Qandil <saqandil@gmail.com>
2018-08-26 14:51:01 -0700

    Adding in my first Dao

---
+++ app/src/main/java/com/teamtreehouse/pizzakeeper/data/ToppingDao.kt
@@ -0,0 +1,20 @@
+package com.teamtreehouse.pizzakeeper.data
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+/**
+ * Created by saqan on 8/26/2018.
+ */
+@Dao
+interface ToppingDao {
+    @Query("select * from topping")
+    fun getAll(): List<Topping>
+
+    @Query("select * from topping where id = :id")
+    fun getToppingById(id: Int): Topping
+    
+    @Insert
+    fun insert(topping: Topping)
+}
